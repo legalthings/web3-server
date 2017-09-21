c4b4ee8e0adca72558377ee167a76c00d4bdaff5
Moesjarraf J <moesjarraf@gmail.com>
2017-09-21 15:33:58 +0200

    Enable cors

--- src/libs/app.ts
+++ src/libs/app.ts
@@ -1,6 +1,7 @@
 // modules
 import * as express from 'express';
 import * as compression from 'compression';
+import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import * as logger from 'morgan';
 import * as errorHandler from 'errorhandler';
@@ -14,6 +15,7 @@ import { contractController } from '../controllers/contract';
 // express
 const app = express();
 app.use(compression());
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
